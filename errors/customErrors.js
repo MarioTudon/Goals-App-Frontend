76a95603506f69abb120dccae649c207735ad384
Mario Tudon <mario.tudon97@gmail.com>
2025-11-05 18:27:17 -0600

    Se definio nombre para errores de API

--- errors/customErrors.js
+++ errors/customErrors.js
@@ -1,17 +1,10 @@
 class APIError extends Error {
   constructor(message, error) {
     super(message)
+    this.name = 'APIError'
     this.error = error
     Error.captureStackTrace(this, this.constructor)
   }
 }
 
-class AppError extends Error {
-  constructor(message, error) {
-    super(message)
-    this.error = error
-    Error.captureStackTrace(this, this.constructor)
-  }
-}
-
-export default { APIError, AppError }
+export default { APIError }
